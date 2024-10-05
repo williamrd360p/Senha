dc0bb4a52da514ae014d18334082459dfdffb104
williamrd360p <e107ti@etec.sp.gov.br>
2024-10-04 22:54:12 -0300

    programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+string senhaDigitada = Console.ReadLine()!;
+const string senhaCorreta = "Corinthians";
+
+bool senhaValida = senhaDigitada == senhaCorreta;
+
+if (senhaValida)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Acesso permitido");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Acesso negado");
+    Console.Beep();
+    Console.Beep();                             
+}
+
+Console.ResetColor();
